2de7c924fc742f6646a16b48b461fa351933c86a
Ammon Lauritzen <allaryin@gmail.com>
2015-05-22 12:54:11 -0500

    Switched flower generation to use bulk wildflower generator.

--- src/main/java/extrabiomes/meadow/handlers/BlockHandler.java
+++ src/main/java/extrabiomes/meadow/handlers/BlockHandler.java
@@ -1,5 +1,11 @@
 package extrabiomes.meadow.handlers;
 
+import java.util.List;
+
+import net.minecraft.item.ItemStack;
+
+import com.google.common.collect.Lists;
+
 import cpw.mods.fml.common.registry.GameRegistry;
 import extrabiomes.lib.items.ExtraItem;
 import extrabiomes.lib.settings.BiomeSettings;
@@ -7,6 +13,7 @@ import extrabiomes.lib.settings.BlockSettings;
 import extrabiomes.lib.worldgen.ExtraWorldGenerator;
 import extrabiomes.lib.worldgen.ExtrabiomeGenBase;
 import extrabiomes.lib.worldgen.WorldGenDecoration;
+import extrabiomes.lib.worldgen.WorldGenWildflower;
 import extrabiomes.meadow.blocks.BlockExtraFlower;
 import extrabiomes.meadow.stuff.BiomeCollection;
 import extrabiomes.meadow.stuff.BlockCollection;
@@ -26,17 +33,20 @@ public abstract class BlockHandler {
 		final ExtrabiomeGenBase meadowBiome = meadowSettings.getBiome().get();
 		
 		// max of 8 attempts per chunk << TODO: make configurable
-		final ExtraWorldGenerator gen = new ExtraWorldGenerator(8);
+		final ExtraWorldGenerator gen = new ExtraWorldGenerator(12);
 		gen.registerBiome(meadowSettings);
-        GameRegistry.registerWorldGenerator(gen, 50);
+        GameRegistry.registerWorldGenerator(gen, 20);
 		
 		final BlockExtraFlower block = new BlockExtraFlower();
 		GameRegistry.registerBlock(block, ExtraItem.class, "meadow.flower");
 		
+		List<ItemStack> flowerStacks = Lists.newArrayList();
 		for( BlockExtraFlower.BlockType type : BlockExtraFlower.BlockType.values() ) {
 			meadowBiome.addFlower(block, type.metadata, type.weight);
-			gen.registerGenerator(type.name(), new WorldGenDecoration(block, type.metadata));
+			flowerStacks.add(new ItemStack(block, 1, type.metadata));
 		}
+		
+		gen.registerGenerator("wildflower", new WorldGenWildflower(flowerStacks));
 		
 		// TODO: register with Forestry
 	}
