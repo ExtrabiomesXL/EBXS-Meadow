9e22e3b4e8b0f230ba5aeddd8a4a3468ef0234c3
Ammon Lauritzen <allaryin@gmail.com>
2015-05-23 20:19:16 -0500

    Bumped version number to 0.0.1

--- src/main/java/extrabiomes/meadow/Version.java
+++ src/main/java/extrabiomes/meadow/Version.java
@@ -5,7 +5,7 @@ import java.util.Locale;
 public abstract class Version {
 	public static final String MOD_ID		= "ExtrabiomesMeadow";
 	public static final String MOD_NAME		= "Extrabiomes Meadow";
-	public static final String VERSION		= "0.0.0";
+	public static final String VERSION		= "0.0.1";
 
 	public static final String LOC_PREFIX   = "extrabiomes.meadow";
 	
